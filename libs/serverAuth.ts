9114f982e39a6d0a7ff2548a0788343a117b5dda
TaiNguyen <ntai8463@gmail.com>
2023-11-18 23:48:33 +0700

    Add Protecting routes and Profiles screen

---
+++ libs/serverAuth.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth";
+
+import prismadb from '@/libs/prismadb';
+// import { authOptions } from "@/pages/api/auth/[...nextauth]";
+
+const serverAuth = async (req: NextApiRequest, res: NextApiResponse) => {
+  // const session = await getServerSession(req, res, authOptions);
+  const session = await getSession({ req });
+
+  if (!session?.user?.email) {
+    throw new Error('Not signed in');
+  }
+
+  const currentUser = await prismadb.user.findUnique({
+    where: {
+      email: session.user.email,
+    }
+  });
+  
+  if (!currentUser) {
+    throw new Error('Not signed in');
+  }
+
+  return { currentUser };
+}
+
+export default serverAuth;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,31 @@
+import { signOut } from 'next-auth/react'
+import { getSession } from 'next-auth/react';
+import useCurrentUser from '@/hooks/useCurrentUser';
+
+// Check user login
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
 export default function Home() {
+  const { data: user } = useCurrentUser()
   return (
     <>
-     <h1>Netflix clone</h1>
+     <h1 className='text-4xl text-green-500'>Netflix clone</h1>
+     <p className='text-white'>Logged is as: {user?.name}</p>
+     <button className='h-10 w-full bg-white' onClick={() => signOut()}>Logout!</button>
     </>
   )
 }
